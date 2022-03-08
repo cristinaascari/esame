3a4c44bcc314810a7a36a02ea8caf1e364b10d9f
cristinaascari <criascari99@gmail.com>
2022-03-08 14:54:43 +0100

    aggiornamento santa claus

--- meta-example/recipes-example/santa-claus/files/SantaClaus.c
+++ meta-example/recipes-example/santa-claus/files/SantaClaus.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+
 int Elfi_con_problemi = 0; /*variabile condivisa tra gli elfi e Babbo Natale per sapere quanti elfi hanno bisogno di aiuto*/
 int Renne_arrivate = 0; /*variabile condivisa tra le renne e Babbo Natale per sapere quante renne dsono arrivate dai tropici*/
 bool Finito = false; /*variabile condivisa tra le renne e Babbo Natale per sapere se sono arrivate tutte e nove le renne, in queso modo Babbo Natale sa quando può terminare la sua esecuzione*/
@@ -11,35 +13,42 @@ sem_t Babbo_Natale; /*semaforo sul quale Babbo Natale si addormenta in attesa di
 sem_t Renne; /*semaforo sul quale le renne si fermano per aspettare che tutte e nove siano arrivate*/
 sem_t Elfi; /*semaforo sul quale si fermano gli elfi che attendono il ritorno dei 3 elfi partiti*/
 sem_t Help; /*semaforo sul quale si fermano gli elfi in attesa del proprio turno di essere aiutati*/
+int elfo[3]={-1,-1,-1}; /* buffer per tenere traccia degli elfi che sono stati aiutati da babbo natale*/
+int punto=0; /* per tenere l'indice del buffer */
+
+
 void PrepareSleigh(){
-	printf("*** Sono Babbo Natale, sono arrivate tutte e nove le renne e quindi inizio a preparare la slitta *** \n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("*** Sono Babbo Natale: TUTTE e nove le RENNE SONO ARRIVATE: LA SLITTA PARTE!!! *** \n");
 	for(int k = 0 ; k < 9 ; ++k){
 		sem_post(&Renne);
 	}
 }
 
 void HelpElves(){
-	printf("*** Sono Babbo Natale, sono arrivati tre elfi che hanno bisogno di aiuto con la preparazione dei giocattoli **************************************** \n");
+	printf("*** Sono Babbo Natale: sto aiutanto TRE elfi ( %d, %d e %d) per creare i giocattoli *** \n", elfo[0], elfo[1], elfo[2]);
+	punto=0;
 	for(int k = 0 ; k < 3 ; ++k){
 		sem_post(&Help);
 	}
 }
 
 void GetHelp(int i){
-	printf("--- Sono l' elfo numero %d e sono stato aiutato ------------------------------------------------------------------------------ \n", i - 9);
+	printf("--- Sono l'elfo %d e sono stato aiutato ---\n", i - 9);
 }
 
 void GetHitched(int i){
-	printf("+++ Sono la renna numero %d e sono stata attaccata alla slitta +++ \n", i);
+	printf("+++ Sono la renna %d e sono stata attaccata alla slitta +++ \n", i);
 }
 
 void RICHIEDI_AIUTO(int i){
 		sem_wait(&Elfi);
 		sem_wait(&mutex);
 		Elfi_con_problemi  += 1;
-		printf("sono l'elfo n.%d e ci sono %d elfi da aiutare-----------------------------------------------------------------------------------------\n", i, Elfi_con_problemi);
+		elfo[punto]=i;
+		punto++;
 		if(Elfi_con_problemi == 3){
-			printf("sono l'elfo n.%d e ho svegliato Babbo natale-----------------------------------------------------------------------------------------\n", i);
+			printf("--- Sono l'elfo %d, sono il terzo e quindi sveglio Babbo natale ---\n", i);
 			sem_post(&Babbo_Natale);
 		}else{
 			sem_post(&Elfi);
@@ -68,21 +77,25 @@ void *esegui_Elfo(void *id)
 		perror("Problemi con l'allocazione di ptr\n");
 		exit(-1);
 	}
-	printf("Thread%d partito: ho come identificatore %lu, sono un elfo \n", *pi, pthread_self());
+	printf("Il thread %d ELFO %d sta arrivando\n", *pi, *pi-9);
+
 	int n = (rand() % 3) + 1;
-	if(n <= 2){
+	if(n == 1 || n==3){
 		aiuto = true;
-		printf("--- Sono l'elfo n.%d e ho bisogno di aiuto ------------------------------------------------------------------------------------- \n", *pi -9);
+		printf("--- Sono l'elfo %d e ho bisogno di aiuto --- \n", *pi -9);
+	}
+	else if(n==2){
+		printf("---Sono l'elfo %d e NON ho bisogno di aiuto ---\n", *pi -9);
 	}
 
 	if(aiuto == true){		
 		RICHIEDI_AIUTO(*pi -9);
-		printf("sono l'elfo n.%d e ho rilasciato il mutex e mi sto addormentando su Help-----------------------------------------------------------------------------------------\n", *pi -9);
 		sem_wait(&Help);
-		printf("sono l'elfo n.%d e mi hanno svegliato da Help-----------------------------------------------------------------------------------------\n", *pi -9);
 		GetHelp(*pi);
 		RILASCIA_AIUTO(*pi -9);		
 	}
+
+	/* il thread ritorna al padre il suo numero d'ordine*/
 	*ptr = *pi;
 	pthread_exit((void *) ptr);
 }
@@ -97,13 +110,16 @@ void *esegui_Renna(void *id)
 		perror("Problemi con l'allocazione di ptr\n");
 		exit(-1);
 	}
-	printf("Thread%d partito: ho come identificatore %lu, sono una renna \n", *pi, pthread_self());
-	sleep(30);
+	printf("Il thread RENNA %d sta arrivando \n", *pi);
+	int TempoDiRitorno= rand()%5;
+	TempoDiRitorno+=10;
+	printf("Sono la renna %d e sto in vacanza per %d secondi\n", *pi, TempoDiRitorno);
+	sleep(TempoDiRitorno);
 	sem_wait(&mutex);
 	Renne_arrivate += 1;
 	if(Renne_arrivate == 9){
 			sem_post(&Babbo_Natale);
-			printf("+++ Sono l'ultima renna e ho preso Babbo Natale +++ \n");
+			printf("+++ Sono l'ultima renna e sveglio Babbo Natale +++ \n");
 		}
 	sem_post(&mutex);
 	sem_wait(&Renne);
@@ -111,7 +127,7 @@ void *esegui_Renna(void *id)
 	GetHitched(*pi);
 	
 
-
+	/* Il thread ritorna al padre il suo numero d'ordine*/
 	*ptr = *pi;
 	pthread_exit((void *) ptr);
 }
@@ -126,11 +142,11 @@ void *esegui_Babbo_Natale(void *id)
 		perror("Problemi con l'allocazione di ptr\n");
 		exit(-1);
 	}
-	printf("Thread%d partito: ho come identificatore %lu, sono Babbo Natale \n", *pi, pthread_self());
+	printf("il thread BABBO NATALE %d sta arrivando \n", *pi);
 	while(!Finito){
-		printf("sono Babbo Natale e mi sono addormentato-------------------------------------------------------------------------------------------------\n");
+		printf("Sono BABBO NATALE e non ci sono le renne e non ci sono abbastanza elfi: MI ADDORMENTO 'zzz...'\n");
 		sem_wait(&Babbo_Natale);
-		printf("sono Babbo Natale e qualcuno mi ha risvegliato---------------------------------------------------------------------------------------------\n");
+		printf("*** Sono Babbo Natale e qualcuno mi ha risvegliato ***\n");
 		sem_wait(&mutex);
 		if(Renne_arrivate == 9){
 			PrepareSleigh();
@@ -140,6 +156,7 @@ void *esegui_Babbo_Natale(void *id)
 		}
 		sem_post(&mutex);
 	}
+	/* il thread ritorna al padre il suo numero d'ordine*/
 	*ptr = *pi;
 	pthread_exit((void *) ptr);
 }
@@ -156,7 +173,7 @@ int main (int argc, char **argv)
 	/*Deve essere passato esattamente un parametro*/
 	if (argc != 2 ) 
 	{
-		printf(error,"Errore nel numero dei parametri %d\n", argc-1);
+		sprintf(error,"Errore nel numero dei parametri %d\n", argc-1);
 		perror(error);
        		exit(1);
 	}
@@ -168,8 +185,10 @@ int main (int argc, char **argv)
 		perror(error);
         	exit(2);
    	}
+
 	/*Il numero di Pthread da creare deve comprendere anche Babbo Natale e le 9 renne*/
 	NUM_THREADS += 10;
+
 	/*Allochiamo la memoria necessaria ai Pthread*/
 	thread=(pthread_t *) malloc(NUM_THREADS * sizeof(pthread_t));
 	if (thread == NULL)
@@ -183,25 +202,31 @@ int main (int argc, char **argv)
         	perror("Problemi con l'allocazione dell'array taskids\n");
         	exit(4);
     	}
+
+        /* inizializzo i numeri casuali usati per fare la sleep */
+        srand(time(NULL));
+
+
 	/*Creiamo i semafori per accedere alle variabili condivise*/
 	sem_init(&mutex, 0, 1);
 	sem_init(&Babbo_Natale, 0, 0);
 	sem_init(&Renne, 0, 0);
 	sem_init(&Elfi, 0, 1);
 	sem_init(&Help, 0, 0);
+
 	/*Creiamo i Pthread*/
 	for (i=0; i < NUM_THREADS; i++)
    	{
 		if(i==0){
 			taskids[i] = i;
-   			sprintf("Sto per creare il thread %d-esimo\n", taskids[i]);
+   			printf("Sto per creare il thread %d-esimo\n", taskids[i]);
         		if (pthread_create(&thread[i], NULL, esegui_Babbo_Natale , (void *) (&taskids[i])) != 0)
         		{
         	        	sprintf(error,"SONO IL MAIN E CI SONO STATI PROBLEMI DELLA CREAZIONE DEL thread %d-esimo\n", taskids[i]);
         	        	perror(error);
 				exit(5);
         		}
-			printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu(Babbo Natale)\n", i, thread[i]);
+			printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu(BABBO NATALE)\n", i, thread[i]);
 		}else if((1<=i) && (i<=9)){
 			taskids[i] = i;
    			printf("Sto per creare il thread %d-esimo\n", taskids[i]);
@@ -211,7 +236,7 @@ int main (int argc, char **argv)
         	        	perror(error);
 				exit(6);
         		}
-			printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu \n", i, thread[i]);
+			printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu (RENNA)\n", i, thread[i]);
 		}else{
  			taskids[i] = i;
    			printf("Sto per creare il thread %d-esimo\n", taskids[i]);
@@ -221,7 +246,7 @@ int main (int argc, char **argv)
         		        perror(error);
 				exit(7);
         		}
-			printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu\n", i, thread[i]);
+			printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu (ELFO)\n", i, thread[i]);
 		}
     	}
 	/*Il main aspetta le renne e Babbo Natale*/
@@ -234,3 +259,5 @@ int main (int argc, char **argv)
    	} 
    	exit(0);
 }
+
+
